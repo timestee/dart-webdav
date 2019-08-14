f36fe872d2302ac92275c236eb79ae0c53303d9e
Hauke Tönjes <hauke.toenjes99@gmail.com>
2019-08-14 16:58:12 +0200

    - Fix #3 - Rewrote the xmlparser so that it is no longer converting xml to json

--- lib/src/file.dart
+++ lib/src/file.dart
@@ -1,18 +1,35 @@
-import 'dart:convert';
-
-import 'package:xml2json/xml2json.dart';
 import 'package:xml/xml.dart' as xml;
 
 class FileInfo {
-  String name;
+  String path;
   String size;
-  String mtime;
-  String ctime;
+  String modificationTime;
+  DateTime creationTime;
   String contentType;
 
-  FileInfo(this.name, this.size, this.mtime, this.ctime, this.contentType);
+  FileInfo(this.path, this.size, this.modificationTime, this.creationTime,
+      this.contentType);
 
-  bool get isDict => this.contentType == 'httpd/unix-directory';
+  // Returns the decoded name of the file / folder without the whole path
+  String get name {
+    if (this.isDirectory) {
+      return Uri.decodeFull(this.path
+          .substring(0, this.path.lastIndexOf("/"))
+          .split("/")
+          .last);
+    }
+
+    return Uri.decodeFull(this.path
+        .split("/")
+        .last);
+  }
+
+  bool get isDirectory => this.path.endsWith("/");
+
+  @override
+  String toString() {
+    return 'FileInfo{name: $name, isDirectory: $isDirectory ,path: $path, size: $size, modificationTime: $modificationTime, creationTime: $creationTime, contentType: $contentType}';
+  }
 }
 
 /// get filed [name] from the property node
@@ -27,34 +44,43 @@ String prop(dynamic prop, String name, [String defaultVal]) {
   return defaultVal;
 }
 
-/// get file info list from `ls` command response
-List<FileInfo> treeFromWevDavXmlOld(String xmlStr) {
-  final Xml2Json myTransformer = Xml2Json();
-  myTransformer.parse(xmlStr);
-  final jsonResponse = json.decode(myTransformer.toParker());
-  final responses = jsonResponse['D:multistatus']['D:response'];
-  var tree = new List<FileInfo>();
-  if (responses is List) {
-    responses.forEach((response) {
-      final elem = response['D:propstat']['D:prop'];
-      FileInfo f = FileInfo(
-          response['D:href'],
-          prop(elem, 'getcontentlength'),
-          prop(elem, 'getlastmodified'),
-          prop(elem, 'creationdate'),
-          prop(elem, 'getcontenttype'));
-      tree.add(f);
-    });
-  }
-  return tree;
-}
-
 List<FileInfo> treeFromWevDavXml(String xmlStr) {
-  var xmlDocument = xml.parse(xmlStr);
-  var responseList = xmlDocument.findAllElements("D:response").toList();
+  // Initialize a list to store the FileInfo Objects
+  var tree = new List<FileInfo>();
 
-  responseList.forEach((response) {
+  // parse the xml using the xml.parse method
+  var xmlDocument = xml.parse(xmlStr);
+
+  // Iterate over the response to find all folders / files and parse the information
+  xmlDocument.findAllElements("D:response").forEach((response) {
     var davItemName = response.findElements("D:href").single.text;
-    print(davItemName);
+    response
+        .findElements("D:propstat")
+        .single
+        .findElements("D:prop")
+        .forEach((element) {
+      var contentLength =
+          element
+              .findElements("D:getcontentlength")
+              .single
+              .text;
+
+      var lastModified = element
+          .findElements("D:getlastmodified")
+          .single
+          .text;
+
+      var creationTime = element
+          .findElements("D:creationdate")
+          .single
+          .text;
+
+      // Add the just found file to the tree
+      tree.add(new FileInfo(davItemName, contentLength, lastModified,
+          DateTime.parse(creationTime), ""));
+    });
   });
+
+  // Return the tree
+  return tree;
 }
